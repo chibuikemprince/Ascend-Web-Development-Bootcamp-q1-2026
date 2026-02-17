fa69677031e7b7dc767dce69af4575c6f7f4a037
Prince Chisomaga <youngprince042@yahoo.com>
2026-02-17 18:55:12 +0100

    Javascript Day 2

---
+++ javascript/day2.js
@@ -0,0 +1,84 @@
+var number1 = 10
+var number2 = 20
+var number3 = 301
+
+var total = number1 + number2 + number3
+var diff = number3 - number1 - number2
+var product = number1 * number2 * number3
+var divide = number3 / number1
+
+var firstname = "Prince"
+var secondname = "Chisomaga"
+
+var fullname = firstname +" "+ secondname
+
+var mod = number3 % number2
+
+//10 x 10 x 10 x 10 x 10
+var power = 10**5
+
+// number1 = number1 + 20  
+// number1 += 200
+number1 -= 2
+
+// var compare = number1 < number2
+// var compare = number1 > number2
+// var compare = number1 >= 9
+
+// var compare = number1 <= 9
+var a  = 10
+var b = "10"
+
+
+// var compare = a === b
+
+
+// var compare = number1 != 8
+
+// var compare = a !== b
+
+var condition1 = number1 > number2
+var condition2   =  number3 > number1
+
+
+
+// var compare = condition1 && condition2
+
+// var compare = condition1 || condition2
+ 
+ 
+var number4  = number3++;
+// 302
+
+var number6 = number3--
+// 301
+
+var number7 = ++number3
+var number8  = --number3
+
+
+//typeof
+//instanceof
+
+var compare  = !condition1
+
+console.log("Number 4: ", number4)
+console.log("Number 6 ", number6)
+console.log("Number 7: ", number7)
+console.log("Number 8 ", number8)
+
+console.log("Condtion1 :"+ condition1)
+console.log("Condition2: "+ condition2)
+console.log("Compare " + compare)
+
+console.log("Number 1: ", number1)
+/*
+ console.log("POWER: ", power)
+console.log("Mod: "+ mod)
+console.log("Total: "         + total)
+console.log("Difference: "+diff)
+console.log(product)
+console.log(divide)
+*/
+
+console.log(fullname)
